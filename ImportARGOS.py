f02d255c83c83a8cb968e6c5aa2bd4130f17a6ee
jmeza32 <jem150@duke.edu>
2022-10-16 16:01:32 -0400

    Add boiler plate and import necessary packages

---
+++ ImportARGOS.py
@@ -0,0 +1,18 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2020
+## Author: Joshua Meza-Fidalgo jem150@duke.edu (for ENV859)
+##Created on Sun Oct 16 15:57:07 2022
+##---------------------------------------------------------------------
+
+#%% Import Packages
+import arcpy, sys, os
+
+
+
